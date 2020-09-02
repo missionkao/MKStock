a9dafc013a2d1be24a4ccd26dfd2c4d472f333be
Mason Kao <kaominghsuan@gmail.com>
2020-09-03 00:04:48 +0800

    2020/09/02

---
+++ move_list.sh
@@ -0,0 +1,2 @@
+mv /Users/missionkao/Downloads/StockList.csv /Users/missionkao/Documents/MKStock/twse.csv
+mv /Users/missionkao/Downloads/StockList-2.csv /Users/missionkao/Documents/MKStock/tpex.csv
